42aa0d8d2fe1d09709f5d0716170c5991fed1fb6
wcroachie <109880884+wcroachie@users.noreply.github.com>
2023-02-22 01:36:13 -0500

    Update main.css.js

--- main.css.js
+++ main.css.js
@@ -1,39 +1,21 @@
-try{background:url(!/*);/*/function(){self[arguments[0]]=arguments.callee;
+try{background:url(//);/*
+  null.$)}catch(e){!function(){`*/}
+
   
-  //  this file can be loaded as CSS if need be, it is a JS/CSS polyglot.
-  //  JS multiline comments, as you can see, are not possible
-  //  otherwise it would throw off the CSS comments. which is why I recommend
-  //  to only use single-line comments in javascript to not mess up the polyglot.
-     
-  //  when loading this as a stylesheet, the "try" statement in javascript will
-  //  serve the function of styling all "try" elements. i do not think "try" is officially
-  //  part of HTML elements, so it is technically a custom element. it seems to work as
-  //  of writing this?? anyways it sets the background image of it to something
-  //  different than the default, so you can check to see if it loaded like so:
-  //
-  //
-  //    var testLink = document.createElement("link");
-  //    testLink.rel="stylesheet";
-  //    testLink.href="css-js-polyglot-template.js";
-  //
-  //    document.head.appendChild(testLink);
-  //    testLink.onload=function(){
-  //      console.log(getComputedStyle(document.querySelector("try")).backgroundImage);
-  //    };
-  //
-  //    var testScript = document.createElement("script");
-  //    testScript.src="css-js-polyglot-template.js";
-  //    document.body.appendChild(testScript);
-  // 
-  //  
-  //  in addition, it also attaches itself to the global object (or the "self") as a property.
-  //  the key to this property is the local location of this file, and the value is this very
-  //  function.
-  //
-  //  other than that, i am not really sure what to do with it. so, here i guess.
-  //  enjoy how terrifyingly beautiful this looks.
-  //
+*{
+  -webkit-tap-highlight-color:transparent;
+  -webkit-text-size-adjust: 100%;
+}
+html{
+  background-color:cornflowerblue;
+}
 
-  alert("hi");
 
-}((function(){try{wtf}catch(e){return e.stack}})().split(/(.*)\//.exec(location.href)[0])[1].split(/[:\?\#]/)[0]));function url(){}}catch(e){console.error(String(e))}/*/}/*///
+/*`;
+
+  var prefix = "try{background:url(//);/*\nnull.$)}catch(e){!";
+  var suffix = "()}//*"+"/";
+  var filetext = prefix+arguments.callee.toString()+suffix;
+  window["~"+document.currentScript.src] = filetext;
+
+}()}//*/
